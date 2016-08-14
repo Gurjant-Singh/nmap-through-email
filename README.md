736f790b92dd0eb4aa7b5f9f7bfc4f8c0878856c
Gurjant <gurjant31@gmail.com>
2016-08-15 03:01:33 +0530

    Update

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# nmap-through-email
+# Nmap Through Email
+
+Nmap through email is script develop to help security researcher. Commands to scan ip address can be send over the email and can recieve the result by email.  
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisities
+
+Python Libraries and tools required
+
+```
+[1]Nmap
+[2]Python Libraries:-
+	mail, getpass, imaplib, os, subprocess, random, thread, MIMEText, 
+	MIMEApplication, MIMEMultipart, SMTP, smtplib, sys
+```
+
+### Installing
+
+Make sure to install the nmap on the system. If script unable to find the nmap, please paste it in same folder as nmap
+
+Starting the Script
+
+```
+[1]Enter the emails address from which script recieve the emails
+[2]Provide the password.
+[3]Rest will be handel by script
+```
+
+Warning
+
+```
+Make sure the email account has access through smtp and imap
+```
+
+## Request for scan
+
+While sending request for nmap scan make sure to follow the following format
+
+### Email Format
+
+To add switch option to nmap,make sure to follow the format. For example
+```
+Subject of email:-  nmap -sV -A
+
+Warning: Do not add any input or out switch in subject
+```
+
+To add Ip or domain list to nmap scan, make sure to follow the format. For example
+```
+Body of email:-  ip={192.168.1.1,192.168.1.2} OR
+				 ip={scanme.nmap.org,nmap.org}
+```
+### Receiving Result 
+
+Result will be recieved in 3 files. 
+-Input file (Contain the scan list of ips/domains)
+-Output file (Contain the result of scan in text file)
+-Outout file (Contain the result as XML format)
+
+## Authors
+
+* **Gurjant Singh** - *Security Researcher*
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE.md](LICENSE.md) file for details
---
+++ nmapscanbyemail.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+#Imports
+import email, getpass, imaplib, os , subprocess, random, thread, sys, smtplib
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+
+#User Input
+user = raw_input("Enter your GMail username:")
+pwd = getpass.getpass("Enter your password: ")
+detach_dir = '.'
+
+#If something goes wrong.
+def something_wrong(From):
+	print '[!]Something Worng..'
+	subject="Aler! Something Worng"
+	message="Hi, \nThere is some problem with Script.\nMight be problem with format. Send 'nmap help' as subject in email for help.\nOtherwise, Please contact the admin for solution\nThanks"
+	send_help(From,subject,message)
+
+#How to use, Reply back
+def send_help(From,subject,message):
+	print '[*]Sending help back...'
+	try:
+		emaillist = From
+		msg = MIMEMultipart()
+		msg['Subject'] = subject
+		msg['From'] = user
+		msg['Reply-to'] = From
+		 
+		msg.preamble = 'Multipart massage.\n'
+		 
+		part = MIMEText(message)
+		msg.attach(part)
+		 
+		server = smtplib.SMTP("smtp.gmail.com:587")
+		server.ehlo()
+		server.starttls()
+		server.login(user, pwd)
+		server.sendmail(msg['From'], emaillist , msg.as_string())
+	except:
+		print "[!]Some problem with sending help"
+
+#Deleting files 
+def deletefile(filename,result_filename1,result_filename2):
+	try:
+		os.remove('./'+filename)
+		os.remove('./'+result_filename1)
+		os.remove('./'+result_filename2)
+	except:
+		print "[!]Problem with deleting files"
+
+#Creating a file for nmap input
+def wirte_tofile(iplist,subject,From):
+	print '[*]Creating a file..'
+	print iplist
+	filename= 'input_'+str(random.randrange(1,1000000,3))+'.txt'
+	with open(filename, 'w') as output:
+		for item in iplist:
+			output.write(item + '\n')		
+	startscan(subject,From,filename)
+
+#Checking email address for ip/domain list
+def find_ipaddress(mail,subject,From):
+	if 'ip={' in mail:
+		print '[*]Checking for ip address'
+		start_iplist=mail.index("ip={")+4
+		raw_list=str(mail[start_iplist:len(mail)])
+		end_iplist=0
+		for i in raw_list:
+			if i == '}':
+				break
+			else:
+				end_iplist=end_iplist+1
+		iplist=raw_list[0:end_iplist]
+		iplist=iplist.split(',')
+		print '[*]Ip list found'
+		wirte_tofile(iplist,subject,From)
+	else:
+		print '[!]No list found'
+		something_wrong(From)
+
+#Sending scan result back
+def send_result(subject,From,filename,result_filename1,result_filename2):
+	print '[*]Sending result back...'
+	try:
+		emaillist = From
+		msg = MIMEMultipart()
+		msg['Subject'] = 'Result for nmap scan ' + subject
+		msg['From'] = user
+		msg['Reply-to'] = From
+		 
+		msg.preamble = 'Multipart massage.\n'
+		 
+		part = MIMEText("Hi, please find the attached file for results")
+		msg.attach(part)
+		 
+		part = MIMEApplication(open(result_filename1,"rb").read())
+		part.add_header('Content-Disposition', 'attachment', filename=result_filename1)
+		msg.attach(part)
+
+		part = MIMEApplication(open(result_filename2,"rb").read())
+		part.add_header('Content-Disposition', 'attachment', filename=result_filename2)
+		msg.attach(part)
+
+		part = MIMEApplication(open(filename,"rb").read())
+		part.add_header('Content-Disposition', 'attachment', filename=filename)
+		msg.attach(part)
+		 
+		server = smtplib.SMTP("smtp.gmail.com:587")
+		server.ehlo()
+		server.starttls()
+		server.login(user, pwd)
+		 
+		server.sendmail(msg['From'], emaillist , msg.as_string())
+		print '[*]Sending Compelete..'
+		print '[*]Deleting files..'
+		deletefile(filename,result_filename1,result_filename2)
+		print '[*]Checking New Emails..'
+	except:
+		print "[!]Some problem with sending result back"
+
+#Starting nmap scan
+def startscan(subject,From,filename):
+	try:
+		print '[*]Starting Namp Scan'
+		result_filename1= 'result_xml_'+str(random.randrange(1,1000000,2))+'.xml'
+		result_filename2= 'result_text_'+str(random.randrange(1,1000000,3))+'.txt'
+		command='sudo '+subject+' -iL '+'./'+str(filename)+' -oX '+result_filename1 +' -o '+result_filename2
+		process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+		process.wait()
+		send_result(subject,From,filename,result_filename1,result_filename2)
+	except:
+		print "hi"
+
+#Check for new emails from email id.
+def checkemails(m):
+	print '[*]Checking for new emails'
+	while True:
+		m.select("Inbox")
+		resp, items = m.search(None, '(UNSEEN)') 
+		items = items[0].split() 
+
+		if items != None:
+			for emailid in items:
+			    resp, data = m.fetch(emailid, "(RFC822)") 
+			    email_body = data[0][1]
+			    mail = email.message_from_string(email_body) 
+
+			    print "["+mail["From"]+"] :" + mail["Subject"]
+			    subject=mail["Subject"]
+			    From=mail["From"]
+			    raw_text=str(mail)
+			    try:
+				    if subject=='nmap help':
+				    	subject="How to use Email-nmap"
+				    	message="Hi, \nFollow the instruction to use Email-nmap\n[1]Add the nmap and switch you want to use in subject.Example:- nmap -sS -Vv (Don't add any input or output switch)\n[2]Add the ip list in message.Example:- ip={192.168.1.1,192.168.1.2} (follow the format)\nEnjoy."
+				    	send_help(From,subject,message)
+				    else:
+				    	thread.start_new_thread ( find_ipaddress, (raw_text,subject,From) )
+			    except:
+					something_wrong(From)
+		else:
+			print ""
+
+
+def main():
+	try:
+
+		m = imaplib.IMAP4_SSL("imap.gmail.com")
+		m.login(user,pwd)
+
+		print '[*]Login Succesful'
+
+	except:
+		print '[*]Unable to Login'
+
+	checkemails(m)
+
+main()
+
